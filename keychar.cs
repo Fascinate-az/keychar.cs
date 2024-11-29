32509c45c743c7c5fa1c6db5506d21abde38c646
Fascinate <eddo92@yahoo.it>
2024-11-29 18:10:22 +0100

    Create keychar.cs

---
+++ keychar.cs
@@ -0,0 +1,17 @@
+string input = "";
+
+while (true)
+{
+    var tasto = Console.ReadKey();
+
+    if (tasto.Key == ConsoleKey.Enter) 
+    {
+       
+        break;
+    }
+    input += tasto.KeyChar;
+
+}
+Console.WriteLine($"i tasti digitati sono {input}");
+
+Console.ReadKey();
