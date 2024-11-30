76f467e0b9aa112b3169210daf5a47a65cacbb93
Fascinate <eddo92@yahoo.it>
2024-11-30 16:35:33 +0100

    Update keychar.cs

--- keychar.cs
+++ keychar.cs
@@ -1,22 +1,13 @@
-var contatore = 0;
 string input = "";
-
 while (true)
 {
     var tasto = Console.ReadKey();
-    if (tasto.Key == ConsoleKey.A)
+    if (tasto.Key == ConsoleKey.Enter) 
     {
-        contatore++;
-    }
-    else if (tasto.Key == ConsoleKey.Enter)
-    {
+       
         break;
     }
-
-
     input += tasto.KeyChar;
-
 }
-
-Console.WriteLine($"hai digitato A ben {contatore} volte \n le lettere digitate sono {input}");
+Console.WriteLine($"i tasti digitati sono {input}");
 Console.ReadKey();
